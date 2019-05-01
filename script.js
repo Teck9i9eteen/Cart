aaa68af176cc72a70604fcac0d2391bf501782f2
Teck9i9eteen <43444429+Teck9i9eteen@users.noreply.github.com>
2019-05-01 09:51:28 -0400

    Created using Popcode: https://popcode.org

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-var name = $("#URL").val();
-var name = $("#Price").val();
-var name = $("#Name").val();
+var name = $(".URL").val();
+var name = $(".Price").val();
+var name = $(".Name").val();
 
 
 $(".AddMe").click(function() {
-    $(
-}
+    $("img").attr("https://img2.cgtrader.com/items/657782/f160bb8a6a/katana-sakura-model-and-textures-3d-model-low-poly-obj-fbx.png");
+});
+
